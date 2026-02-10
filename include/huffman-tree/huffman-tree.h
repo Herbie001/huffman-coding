a57092341a4eccef603e8ed6a801cab0fd909323
Eric Hebert <ehebert@ucsd.edu>
2026-02-09 22:39:41 -0800

    Updated huffman-tree directory

--- include/huffman-tree/huffman-tree.h
+++ include/huffman-tree/huffman-tree.h
@@ -1,6 +1,7 @@
 #ifndef HUFFMAN_TREE_H
 #define HUFFMAN_TREE_H
 #include "nodes.h"
+#include "io/file-handler.h"
 #include <queue>
 #include <vector>
 #include <fstream>
@@ -16,9 +17,14 @@ class HuffmanTree {
         HuffmanTree() = default;
         ~HuffmanTree();
         void generate(const std::vector<int>& freqs);
-        void encode();
-        void decode();
-        
+        void encode(
+            std::vector<int>& freqs, 
+            std::ofstream& input_file, 
+            std::ofstream& output_file, 
+            Node* root);
+        void decode(
+            std::ofstream& input_file, 
+            std::ofstream& output_file);
 };
 
 #endif
